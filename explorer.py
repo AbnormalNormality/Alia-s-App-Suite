13a732461d1e96f6c5557a7595cc402f04922877
AliaNormality <dan.driscoll@aussiebb.com.au>
2024-09-30 23:49:24 +1000

    Updates link colours. Added open in default app to right-butotn menu.

--- explorer.py
+++ explorer.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from os.path import isdir, basename, join, isfile, dirname, exists, expanduser
-from os import listdir, stat, makedirs
+from os import listdir, stat, makedirs, rename
 from tkinter.messagebox import showerror
 from math import floor, log
 from magic import Magic
 from json import dump, load as load_json
+from subprocess import run
 
 import suite_functions as x_fun
 from suite_functions import *
@@ -21,12 +22,12 @@ x_fun.main = main
 initiate_grid()
 
 
-def try_open(path):
+def try_open(path=None):
+    if path is None:
+        path = user_settings["current_dir"]
+
     def open_path():
         if isdir(path):
-            display_path.set(path)
-            user_settings["current_dir"] = path
-
             children = explorer.winfo_children()
             contents = [f for f in listdir(path) if isdir(join(path, f))] + ["|s"] + \
                        [f for f in listdir(path) if isfile(join(path, f))]
@@ -35,25 +36,33 @@ def try_open(path):
                 contents.insert(0, dirname(path))
 
             class Link(Label):
-                def __init__(self, f_path):
+                def __init__(self, f_path, index_):
+                    self.f_path = f_path
+
                     name = basename(f_path) if basename(f_path) else f_path
 
                     super().__init__(explorer, text=". . ." if f_path == dirname(path) else f"{"> " if isdir(f_path) 
                                      else ""}{name}", background=explorer.cget("background"), justify="left",
-                                     foreground="#505050" if isdir(f_path) and f_path != dirname(path) else "#000000")
+                                     foreground="#a0a0a0" if not can_open(f_path) else
+                                     "#505050" if isdir(f_path) and f_path != dirname(path) else "#000000")
                     self.pack(anchor="w", padx=(10, 0))
-                    self.bind("<Enter>", lambda _: self.configure(background="#ffff00"))
-                    self.bind("<Leave>", lambda _: self.configure(background=explorer.cget("background")))
 
                     if isdir(f_path):
                         self.bind("<Double-Button-1>", lambda _: try_open(f_path))
-                        self.bind("<Button-1>", lambda _: try_preview(f_path))
-                        self.bind("<Button-3>", lambda _: try_menu(f_path))
+                        self.bind("<Button-1>", lambda _: try_preview(f_path) if selected != self.f_path else
+                                  try_open(f_path))
+                        self.bind("<Button-3>", lambda _: try_menu(f_path, self))
 
                     else:
                         self.bind("<Double-Button-1>", lambda _: try_open(f_path))
-                        self.bind("<Button-1>", lambda _: try_preview(f_path))
-                        self.bind("<Button-3>", lambda _: try_menu(f_path))
+                        self.bind("<Button-1>", lambda _: try_preview(f_path) if selected != self.f_path else
+                                  try_open(f_path))
+                        self.bind("<Button-3>", lambda _: try_menu(f_path, self))
+                        main.bind("<F2>", lambda event: try_menu(f_path, self, "rename") if
+                                  main.winfo_containing(event.x_root, event.y_root) == self else None, add=index_ != 1)
+
+                    for j in ["<Enter>", "<Leave>", "<Button-1>"]:
+                        self.bind(j, lambda _: check_all_widgets(), add=True)
 
                     tooltip = [f"Name: {name}"]
 
@@ -67,6 +76,20 @@ def try_open(path):
 
                     ToolTip(self, "\n".join(tooltip), wraplength=350)
 
+                def highlight_colour(self):
+                    hovered_over = main.winfo_containing(*main.winfo_pointerxy()) == self
+                    is_selected = selected == self.f_path
+                    accessible = can_open(self.f_path)
+
+                    colours = {
+                        (True, False): "#c0c0c0",
+                        (False, True): "#ffff00",
+                        (True, True): "#cccc00",
+                        (False, False): explorer.cget("background")
+                    }
+
+                    return colours[(is_selected, hovered_over)]
+
             for i in range(max(len(children), len(contents))):
                 if len(children) > i:
                     children[i].destroy()
@@ -76,9 +99,14 @@ def try_open(path):
                         Border(frame_kwargs={"master": explorer}).pack(fill="x", pady=5)
 
                     else:
-                        Link(join(path, contents[i]))
+                        Link(join(path, contents[i]), i)
+
+            check_all_widgets()
 
             explorer.canvas.yview_moveto(0.0)
+
+            display_path.set(path)
+            user_settings["current_dir"] = path
 
         else:
             print(f"Opening {basename(path)}")
@@ -91,6 +119,10 @@ def try_open(path):
 
 def try_preview(path):
     def preview_path():
+        global selected
+
+        selected = path
+
         children = preview.winfo_children()
         widgets = {
             Label(preview, text=f"{basename(path)}", font="TkDefaultFont 12", wraplength=preview.winfo_width()): {}
@@ -112,14 +144,16 @@ def try_preview(path):
 
             if file_extension in [".txt", ".md", ".py", ".json", ".bat"]:
                 file_contents = open(path, "r", encoding="utf-8").read()
-                char_limit = 250
-                if len(file_contents) <= char_limit:
-                    formatted_contents = file_contents[:char_limit] + "..." if len(file_contents) > char_limit else ""
+
+                char_limit = user_settings["char_limit"]
+
+                if len(file_contents) > char_limit:
+                    formatted_contents = file_contents[:char_limit] + "..."
                 else:
                     formatted_contents = file_contents
 
                 widgets |= {
-                    Border({"master": preview}): {"pady": 10, "fill": "x"},
+                    Border({"master": preview}): dict(pady=10, fill="x"),
                     Label(preview, text=formatted_contents, wraplength=preview.winfo_width(), padx=5, justify="left",
                           background="#ffffff"): {}
                 }
@@ -152,19 +186,55 @@ def convert_size(size_bytes):
     return "%s %s" % (s, size_name[i])
 
 
-def try_menu(path):
+def try_menu(path, link, command=None):
     def open_menu():
         menu = Toplevel()
         menu.geometry(f"+{main.winfo_pointerx()}+{main.winfo_pointery()}")
         menu.overrideredirect(True)
         menu.focus_force()
         menu.bind("<FocusOut>", lambda _: menu.destroy())
+        menu.bind("<ButtonRelease-1>", lambda _: menu.destroy())
+
+        def f_rename():
+            new_name = StringVar(value=basename(path))
+            entry = Entry(explorer, textvariable=new_name, background="#fafafa")
+            entry.pack(after=link, anchor="w", padx=(10, 0))
+            link.destroy()
+            entry.focus_force()
+
+            if "." in new_name.get():
+                entry.icursor(new_name.get().rfind("."))
+                entry.selection_to(new_name.get().rfind("."))
+            else:
+                entry.icursor(len(new_name.get()))
+                entry.selection_to(len(new_name.get()))
+
+            def do_rename():
+                rename(path, join(dirname(path), new_name.get()))
+                try_open()
+
+            entry.bind("<Return>", lambda _: do_rename())
+            main.bind("<Button-1>", lambda event: try_open() if event.widget != entry else None)
+            entry.bind("<Escape>", lambda _: try_open())
+            entry.bind("<F2>", lambda _: try_open())
+            entry.bind("<Right>", lambda _: (entry.icursor(entry.index("insert") - 1), entry.unbind("<Right>"),
+                                             entry.unbind("<Left>")))
+            entry.bind("<Left>", lambda _: (entry.icursor(entry.index("insert") + 1), entry.unbind("<Right>"),
+                                            entry.unbind("<Left>")))
+
+        Button(menu, text="Rename", command=f_rename).pack(fill="x")
+
+        Button(menu, text="Open in Default App", command=lambda: run(["start", "", path], shell=True)).pack(fill="x")
 
         if isdir(path):
             pass
 
         else:
             pass
+
+        if command == "rename":
+            f_rename()
+            menu.destroy()
 
     try:
         open_menu()
@@ -195,10 +265,32 @@ def load():
 
         return dct
 
-    if exists(join(master_folder_path, "user_settings.json")):
+    if (exists(join(master_folder_path, "user_settings.json")) and
+            open(join(master_folder_path, "user_settings.json")).read()):
         user_settings |= load_json(open(join(master_folder_path, "user_settings.json")), object_hook=settings_parser)
 
     try_open(user_settings["current_dir"])
+
+
+# noinspection PyUnresolvedReferences
+def check_all_widgets():
+    [w.configure(**{"background": w.highlight_colour()}) for w in explorer.winfo_children() if
+     hasattr(w, "highlight_colour")]
+
+
+def can_open(path):
+    if isdir(path):
+        try:
+            listdir(path)
+            return True
+        except PermissionError:
+            return False
+    else:
+        try:
+            open(path)
+            return True
+        except (PermissionError, OSError):
+            return False
 
 
 #
@@ -232,7 +324,8 @@ master_folder_path = join(expanduser("~"), "AppData", "Local", "Alia's App Suite
 if not exists(master_folder_path):
     makedirs(master_folder_path, exist_ok=True)
 
-user_settings = {"current_dir": "C:\\"}
+user_settings = {"current_dir": "C:\\", "char_limit": 200}
+selected = ""
 
 load()
 
